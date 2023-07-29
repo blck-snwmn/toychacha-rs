a78335e68679ae7d7010a8d13ba964d97fdf43d0
blck-snwmn <whitesnowmancreator@gmail.com>
2023-07-29 22:49:39 +0900

    Fix lint

--- src/chacha.rs
+++ src/chacha.rs
@@ -1,8 +1,8 @@
-struct state {
+struct State {
     x: [u32; 16],
 }
 
-impl state {
+impl State {
     fn quarter_round(&mut self, ai: usize, bi: usize, ci: usize, di: usize) {
         let a = self.x[ai];
         let b = self.x[bi];
@@ -78,7 +78,7 @@ mod tests {
 
     #[test]
     fn test_state_quarter_round() {
-        let mut s = state {
+        let mut s = State {
             x: [
                 0x879531e0, 0xc5ecf37d, 0x516461b1, 0xc9a62f8a, 0x44c20ef3, 0x3390af7f, 0xd9fc690b,
                 0x2a5f714c, 0x53372767, 0xb00a5631, 0x974c541a, 0x359e9963, 0x5c971061, 0x3d631689,
@@ -88,7 +88,7 @@ mod tests {
 
         s.quarter_round(2, 7, 8, 13);
 
-        let want = state {
+        let want = State {
             x: [
                 0x879531e0, 0xc5ecf37d, 0xbdb886dc, 0xc9a62f8a, 0x44c20ef3, 0x3390af7f, 0xd9fc690b,
                 0xcfacafd2, 0xe46bea80, 0xb00a5631, 0x974c541a, 0x359e9963, 0x5c971061, 0xccc07c79,
@@ -100,7 +100,7 @@ mod tests {
 
     #[test]
     fn test_inner_block() {
-        let mut s = state {
+        let mut s = State {
             x: [
                 0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504, 0x0b0a0908,
                 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c, 0x00000001, 0x09000000,
@@ -112,7 +112,7 @@ mod tests {
             s.inner_block();
         }
 
-        let want = state {
+        let want = State {
             x: [
                 0x837778ab, 0xe238d763, 0xa67ae21e, 0x5950bb2f, 0xc4f2d0c7, 0xfc62bb2f, 0x8fa018fc,
                 0x3f5ec7b7, 0x335271c2, 0xf29489f3, 0xeabda8fc, 0x82e46ebd, 0xd19c12b4, 0xb04e16de,
