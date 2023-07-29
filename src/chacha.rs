fc7128a69683b7d693f5e1730688daa62d09eeff
blck-snwmn <whitesnowmancreator@gmail.com>
2023-07-29 23:59:39 +0900

    Add block

--- src/chacha.rs
+++ src/chacha.rs
@@ -1,3 +1,4 @@
+#[derive(Clone, Debug, PartialEq, Eq)]
 struct State {
     x: [u32; 16],
 }
@@ -43,6 +44,12 @@ impl State {
         self.quarter_round(2, 7, 8, 13);
         self.quarter_round(3, 4, 9, 14);
     }
+
+    fn add(&mut self, other: &State) {
+        for i in 0..16 {
+            self.x[i] = self.x[i].wrapping_add(other.x[i]);
+        }
+    }
 }
 
 struct Chacha {
@@ -53,6 +60,13 @@ impl Chacha {
     fn new(key: [u8; 32], nonce: [u8; 12], counter: u32) -> Self {
         let state = State::new(key, nonce, counter);
         Chacha { state }
+    }
+    fn block(&mut self) {
+        let mut state = self.state.clone();
+        for _ in 0..10 {
+            state.inner_block();
+        }
+        self.state.add(&state)
     }
 }
 
@@ -165,6 +179,32 @@ mod tests {
                     0x61707865, 0x3320646e, 0x79622d32, 0x6b206574, 0x03020100, 0x07060504,
                     0x0b0a0908, 0x0f0e0d0c, 0x13121110, 0x17161514, 0x1b1a1918, 0x1f1e1d1c,
                     0x00000001, 0x09000000, 0x4a000000, 0x00000000,
+                ],
+            },
+        };
+        assert_eq!(c.state.x, want.state.x);
+    }
+
+    #[test]
+    fn test_chacha_block() {
+        let mut c = Chacha::new(
+            [
+                0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
+                0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,
+                0x1c, 0x1d, 0x1e, 0x1f,
+            ],
+            [
+                0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00,
+            ],
+            1,
+        );
+        c.block();
+        let want = Chacha {
+            state: State {
+                x: [
+                    0xe4e7f110, 0x15593bd1, 0x1fdd0f50, 0xc47120a3, 0xc7f4d1c7, 0x0368c033,
+                    0x9aaa2204, 0x4e6cd4c3, 0x466482d2, 0x09aa9f07, 0x05d7c214, 0xa2028bd9,
+                    0xd19c12b5, 0xb94e16de, 0xe883d0cb, 0x4e3c50a2,
                 ],
             },
         };
