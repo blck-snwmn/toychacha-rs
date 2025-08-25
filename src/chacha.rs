75e28235e63108b3c6c35c9ff2f0117e87b53f64
blck-snwmn <whitesnowmancreator@gmail.com>
2025-08-25 11:29:15 +0900

    Optimize State::new with reference parameters

--- src/chacha.rs
+++ src/chacha.rs
@@ -4,8 +4,7 @@ pub struct State {
 }
 
 impl State {
-    pub fn new(key: [u8; 32], nonce: [u8; 12], counter: u32) -> Self {
-        // TODO change args type to reference
+    pub fn new(key: &[u8; 32], nonce: &[u8; 12], counter: u32) -> Self {
         let mut tkey = [0u32; 8];
         for i in 0..8 {
             let bytes = [key[4 * i], key[4 * i + 1], key[4 * i + 2], key[4 * i + 3]];
@@ -123,7 +122,7 @@ impl ChaCha20 {
         let mut hp = plaintext;
 
         while !hp.is_empty() {
-            let mut state = State::new(self.key, self.nonce, counter); // TODO reuse state
+            let mut state = State::new(&self.key, &self.nonce, counter); // TODO reuse state
             let block = state.block();
 
             let len = std::cmp::min(hp.len(), 64);
@@ -210,12 +209,12 @@ mod tests {
     #[test]
     fn test_state_new() {
         let c = State::new(
-            [
+            &[
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
                 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,
                 0x1c, 0x1d, 0x1e, 0x1f,
             ],
-            [
+            &[
                 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00,
             ],
             1,
@@ -233,12 +232,12 @@ mod tests {
     #[test]
     fn test_state_block() {
         let mut c = State::new(
-            [
+            &[
                 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,
                 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b,
                 0x1c, 0x1d, 0x1e, 0x1f,
             ],
-            [
+            &[
                 0x00, 0x00, 0x00, 0x09, 0x00, 0x00, 0x00, 0x4a, 0x00, 0x00, 0x00, 0x00,
             ],
             1,
